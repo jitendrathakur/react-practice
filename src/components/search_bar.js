d9f93dee794ba97202ca79346402171bd2d1746e
Jitendra Thakur <jitendra.thakur@impetus.co.in>
2018-04-20 18:46:34 +0530

    initial react pre requisit

---
+++ src/components/search_bar.js
@@ -0,0 +1,20 @@
+//search
+import React, {Component} from 'react';
+//const Component = React.Component;
+
+// const SearchBar = () => {
+// 	return <input />
+// };
+
+class SearchBar extends Component {
+	render() {
+		//return <input onChange={this.onInputChange} />;
+		return <input onChange={event => console.log(event.target.value)} />;
+	}
+
+	// onInputChange(event) {
+	// 	console.log(event.target.value);
+	// }
+}
+
+export default SearchBar;
